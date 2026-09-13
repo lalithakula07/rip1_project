4c4d832f3e660d3a98443de109c125f34a2c02e6
Lalith Aditya Akula <lalithakula07@gmail.com>
2026-09-13 15:32:48 +0530

    changed name to ayush

--- src/A/Main.java
+++ src/A/Main.java
@@ -5,7 +5,7 @@ import B.Animal;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Helloe");
-        Animal a = new Animal("aryash");
+        Animal a = new Animal("ayush");
         System.out.println(a.getName());
 
 

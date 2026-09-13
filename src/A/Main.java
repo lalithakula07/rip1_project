f9710b8b59aef9e0eec0e01a84ffa6ef0897fae7
Lalith Aditya Akula <lalithakula07@gmail.com>
2026-09-13 15:25:34 +0530

    Initial commit

---
+++ src/A/Main.java
@@ -0,0 +1,13 @@
+package A;
+
+import B.Animal;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Helloe");
+        Animal a = new Animal("aryash");
+        System.out.println(a.getName());
+
+
+    }
+}
